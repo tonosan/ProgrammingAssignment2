1161d0b22142665ba592ae7a98fa7d346ef310a4
gojukaimexico@hotmail.com <gojukaimexico@hotmail.com>
2015-06-20 09:07:08 -0500

    Hi this is my assignment2 hope I have luck

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two function store in a different environment a matrix 
+##and a list of functions and then apply functions to the matrix
+##and display the results
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates a closure an enclosing for de parent function and 
+## allow access to all its variables
 
+makeCacheMatrix <- function(x=matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+   }
+  get<-function() x
+  setmat<-function(solve) m<<-solve
+  getmat<-function() m
+  list(set = set,get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
 
-## Write a short comment describing this function
+## This parent environment of the closure is the execution 
+## environment
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getmat()
+  if(!is.null(m)){
+    message("getting cached matrix")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setmat(m)
+  m
 }
